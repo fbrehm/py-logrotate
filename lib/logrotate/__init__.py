e6de8630337c0b6de574347a4c9ffbe195ff04ff
Frank Brehm <frank.brehm@profitbricks.com>
2016-11-03 14:01:57 +0100

    Adding some meta variables

--- lib/logrotate/__init__.py
+++ lib/logrotate/__init__.py
@@ -3,7 +3,21 @@
 '''
 @author: Frank Brehm
 @contact: frank@brehm-online.com
-@copyright: (c) 2010 - 2011 by Frank Brehm, Berlin
-@summary: All modules for logrotate.py
+@copyright: (c) 2010 - 2016 by Frank Brehm, Berlin
+@summary: All modules for plogrotate
 '''
+
+__author__ = 'Frank Brehm <frank@brehm-online.com>'
+__copyright__ = '(C) 2010 - 2016 by Frank Brehm, Berlin'
+__contact__ = 'frank@brehm-online.com'
+__version__ = '0.7.0'
+__license__ = 'GPLv3+'
+
+# =============================================================================
+
+if __name__ == "__main__":
+
+    pass
+
+
 # vim: fileencoding=utf-8 filetype=python ts=4
