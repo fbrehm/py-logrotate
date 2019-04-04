d8bf24d6ebf8710f5c5cc32093292761f1227290
Frank Brehm <frank.brehm@pixelpark.com>
2019-04-04 17:16:58 +0200

    Removing complexity from functions in lib/logrotate/common.py

--- lib/logrotate/common.py
+++ lib/logrotate/common.py
@@ -28,7 +28,7 @@ from .errors import UnbalancedQuotesError
 
 from .translate import XLATOR
 
-__version__ = '0.4.4'
+__version__ = '0.4.5'
 
 _ = XLATOR.gettext
 ngettext = XLATOR.ngettext
@@ -51,6 +51,22 @@ RE_EMAIL = re.compile(
 
 RE_YES = re.compile(r'^\s*(?:y(?:es)?|true|on)\s*$', re.IGNORECASE)
 RE_NO = re.compile(r'^\s*(?:no?|false|off)\s*$', re.IGNORECASE)
+
+RE_UNIT_BYTES = re.compile(r'^\s*(?:b(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_KBYTES = re.compile(r'^\s*k(?:[bB](?:[Yy][Tt][Ee])?)?\s*$')
+RE_UNIT_KIBYTES = re.compile(r'^\s*K[Ii]?(?:[bB](?:[Yy][Tt][Ee])?)?\s*$')
+RE_UNIT_MBYTES = re.compile(r'^\s*M(?:B(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_MIBYTES = re.compile(r'^\s*Mi(?:B(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_GBYTES = re.compile(r'^\s*G(?:B(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_GIBYTES = re.compile(r'^\s*Gi(?:B(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_TBYTES = re.compile(r'^\s*T(?:B(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_TIBYTES = re.compile(r'^\s*Ti(?:B(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_PBYTES = re.compile(r'^\s*P(?:B(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_PIBYTES = re.compile(r'^\s*Pi(?:B(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_EBYTES = re.compile(r'^\s*E(?:B(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_EIBYTES = re.compile(r'^\s*Ei(?:B(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_ZBYTES = re.compile(r'^\s*Z(?:B(?:yte)?)?\s*$', re.IGNORECASE)
+RE_UNIT_ZIBYTES = re.compile(r'^\s*Zi(?:B(?:yte)?)?\s*$', re.IGNORECASE)
 
 LOG = logging.getLogger(__name__)
 
@@ -175,7 +191,7 @@ def email_valid(address):
 
 
 # =============================================================================
-def human2bytes(                                                                        # noqa
+def human2bytes(
         value, si_conform=False, use_locale_radix=False, as_float=False, verbose=0):
     """
     Converts the given human readable byte value (e.g. 5MB, 8.4GiB etc.)
@@ -257,40 +273,7 @@ def human2bytes(                                                                
     if not si_conform:
         factor_si = factor_bin
 
-    if re.search(r'^\s*(?:b(?:yte)?)?\s*$', suffix, re.IGNORECASE):
-        factor = 1
-    elif re.search(r'^\s*k(?:[bB](?:[Yy][Tt][Ee])?)?\s*$', suffix):
-        factor = factor_si
-    elif re.search(r'^\s*Ki?(?:[bB](?:[Yy][Tt][Ee])?)?\s*$', suffix):
-        factor = factor_bin
-    elif re.search(r'^\s*M(?:B(?:yte)?)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_si ** 2
-    elif re.search(r'^\s*MiB(?:yte)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_bin ** 2
-    elif re.search(r'^\s*G(?:B(?:yte)?)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_si ** 3
-    elif re.search(r'^\s*GiB(?:yte)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_bin ** 3
-    elif re.search(r'^\s*T(?:B(?:yte)?)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_si ** 4
-    elif re.search(r'^\s*TiB(?:yte)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_bin ** 4
-    elif re.search(r'^\s*P(?:B(?:yte)?)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_si ** 5
-    elif re.search(r'^\s*PiB(?:yte)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_bin ** 5
-    elif re.search(r'^\s*E(?:B(?:yte)?)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_si ** 6
-    elif re.search(r'^\s*EiB(?:yte)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_bin ** 6
-    elif re.search(r'^\s*Z(?:B(?:yte)?)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_si ** 7
-    elif re.search(r'^\s*ZiB(?:yte)?\s*$', suffix, re.IGNORECASE):
-        factor = factor_bin ** 7
-    else:
-        msg = _("Couldn't detect suffix {!r}.").format(suffix)
-        raise ValueError(msg)
-
+    factor = _get_factor_human2bytes(suffix, factor_bin, factor_si)
     if verbose > 4:
         msg = _("Found factor {}.").format(factor)
         LOG.debug(msg)
@@ -304,7 +287,49 @@ def human2bytes(                                                                
 
 
 # =============================================================================
-def period2days(period, use_locale_radix=False, verbose=0):                             # noqa
+def _get_factor_human2bytes(suffix, factor_bin, factor_si):
+
+    factor = 1
+
+    if RE_UNIT_BYTES.search(suffix):
+        factor = 1
+    elif RE_UNIT_KBYTES.search(suffix):
+        factor = factor_si
+    elif RE_UNIT_KIBYTES.search(suffix):
+        factor = factor_bin
+    elif RE_UNIT_MBYTES.search(suffix):
+        factor = (factor_si * factor_si)
+    elif RE_UNIT_MIBYTES.search(suffix):
+        factor = (factor_bin * factor_bin)
+    elif RE_UNIT_GBYTES.search(suffix):
+        factor = (factor_si ** 3)
+    elif RE_UNIT_GIBYTES.search(suffix):
+        factor = (factor_bin ** 3)
+    elif RE_UNIT_TBYTES.search(suffix):
+        factor = (factor_si ** 4)
+    elif RE_UNIT_TIBYTES.search(suffix):
+        factor = (factor_bin ** 4)
+    elif RE_UNIT_PBYTES.search(suffix):
+        factor = (factor_si ** 5)
+    elif RE_UNIT_PIBYTES.search(suffix):
+        factor = (factor_bin ** 5)
+    elif RE_UNIT_EBYTES.search(suffix):
+        factor = (factor_si ** 6)
+    elif RE_UNIT_EIBYTES.search(suffix):
+        factor = (factor_bin ** 6)
+    elif RE_UNIT_ZBYTES.search(suffix):
+        factor = (factor_si ** 7)
+    elif RE_UNIT_ZIBYTES.search(suffix):
+        factor = (factor_bin ** 7)
+    else:
+        msg = _("Couldn't detect suffix {!r}.").format(suffix)
+        raise ValueError(msg)
+
+    return factor
+
+
+# =============================================================================
+def period2days(period, use_locale_radix=False, verbose=0):
     """
     Converts the given string of the form »5d 8h« in an amount of days.
     It raises a ValueError on invalid values.
@@ -359,90 +384,25 @@ def period2days(period, use_locale_radix=False, verbose=0):                     
     if verbose > 3:
         LOG.debug(_("Using radix {!r} ...").format(radix))
 
-    # Search for hours in value
-    pattern = r'(\d+(?:' + radix + r'\d*)?)\s*h(?:ours?)?'
-    if verbose > 4:
-        LOG.debug(_("Pattern {!r}.").format(pattern))
-    match = re.search(pattern, value, re.IGNORECASE)
-    if match:
-        hours_str = match.group(1)
-        if use_locale_radix:
-            hours_str = re.sub(radix, '.', hours_str, 1)
-        hours = float(hours_str)
-        days += hours / 24
-        if verbose > 3:
-            LOG.debug(_("Found {:.2f} hours.").format(hours))
-        value = re.sub(pattern, '', value, re.IGNORECASE)
-    if verbose > 4:
-        LOG.debug(_("Rest after hours: {!r}.").format(value))
+    (d, hours, v) = _get_hours_in_period(value, radix, use_locale_radix, verbose)
+    days += d
+    value = v
 
-    # Search for weeks in value
-    pattern = r'(\d+(?:' + radix + r'\d*)?)\s*w(?:eeks?)?'
-    if verbose > 4:
-        LOG.debug(_("Pattern {!r}.").format(pattern))
-    match = re.search(pattern, value, re.IGNORECASE)
-    if match:
-        weeks_str = match.group(1)
-        if use_locale_radix:
-            weeks_str = re.sub(radix, '.', weeks_str, 1)
-        weeks = float(weeks_str)
-        days += weeks * 7
-        if verbose > 3:
-            LOG.debug(_("Found {:f} weeks.").format(weeks))
-        value = re.sub(pattern, '', value, re.IGNORECASE)
-    if verbose > 4:
-        LOG.debug(_("Rest after weeks: {!r}.").format(value))
+    (d, weeks, v) = _get_weeks_in_period(value, radix, use_locale_radix, verbose)
+    days += d
+    value = v
 
-    # Search for months in value
-    pattern = r'(\d+(?:' + radix + r'\d*)?)\s*m(?:onths?)?'
-    if verbose > 4:
-        LOG.debug(_("Pattern {!r}.").format(pattern))
-    match = re.search(pattern, value, re.IGNORECASE)
-    if match:
-        months_str = match.group(1)
-        if use_locale_radix:
-            months_str = re.sub(radix, '.', months_str, 1)
-        months = float(months_str)
-        days += months * 30
-        if verbose > 3:
-            LOG.debug(_("Found {:f} months.").format(months))
-        value = re.sub(pattern, '', value, re.IGNORECASE)
-    if verbose > 4:
-        LOG.debug(_("Rest after months: {!r}.").format(value))
+    (d, months, v) = _get_months_in_period(value, radix, use_locale_radix, verbose)
+    days += d
+    value = v
 
-    # Search for years in value
-    pattern = r'(\d+(?:' + radix + r'\d*)?)\s*y(?:ears?)?'
-    if verbose > 4:
-        LOG.debug(_("Pattern {!r}.").format(pattern))
-    match = re.search(pattern, value, re.IGNORECASE)
-    if match:
-        years_str = match.group(1)
-        if use_locale_radix:
-            years_str = re.sub(radix, '.', years_str, 1)
-        years = float(years_str)
-        days += years * 365
-        if verbose > 3:
-            LOG.debug(_("Found {:f} years.").format(years))
-        value = re.sub(pattern, '', value, re.IGNORECASE)
-    if verbose > 4:
-        LOG.debug(_("Rest after years: {!r}.").format(value))
+    (d, years, v) = _get_years_in_period(value, radix, use_locale_radix, verbose)
+    days += d
+    value = v
 
-    # At last search for days in value
-    pattern = r'(\d+(?:' + radix + r'\d*)?)\s*(?:d(?:ays?)?)?'
-    if verbose > 4:
-        LOG.debug(_("Pattern {!r}.").format(pattern))
-    match = re.search(pattern, value, re.IGNORECASE)
-    if match:
-        days_str = match.group(1)
-        if use_locale_radix:
-            days_str = re.sub(radix, '.', days_str, 1)
-        days_float = float(days_str)
-        days += days_float
-        if verbose > 3:
-            LOG.debug(_("Found {:f} days.").format(days_float))
-        value = re.sub(pattern, '', value, re.IGNORECASE)
-    if verbose > 4:
-        LOG.debug(_("Rest after days: {!r}.").format(value))
+    (d, v) = _get_days_in_period(value, radix, use_locale_radix, verbose)
+    days += d
+    value = v
 
     # warn, if there is a rest
     if not RE_WS_ONLY.search(value):
@@ -452,6 +412,134 @@ def period2days(period, use_locale_radix=False, verbose=0):                     
         LOG.debug(_("Total {!r} days found.").format(days))
 
     return days
+
+
+# =============================================================================
+def _get_hours_in_period(value, radix, use_locale_radix=False, verbose=0):
+    "Search for hours in value"
+
+    days = 0
+    hours = 0
+
+    pattern = r'(\d+(?:' + radix + r'\d*)?)\s*h(?:ours?)?'
+    if verbose > 4:
+        LOG.debug(_("Pattern {!r}.").format(pattern))
+    match = re.search(pattern, value, re.IGNORECASE)
+    if match:
+        hours_str = match.group(1)
+        if use_locale_radix:
+            hours_str = re.sub(radix, '.', hours_str, 1)
+        hours = float(hours_str)
+        days = hours / 24
+        if verbose > 3:
+            LOG.debug(_("Found {:.2f} hours.").format(hours))
+        value = re.sub(pattern, '', value, re.IGNORECASE)
+    if verbose > 4:
+        LOG.debug(_("Rest after hours: {!r}.").format(value))
+
+    return (days, hours, value)
+
+
+# =============================================================================
+def _get_weeks_in_period(value, radix, use_locale_radix=False, verbose=0):
+    "Search for weeks in value"
+
+    days = 0
+    weeks = 0
+
+    pattern = r'(\d+(?:' + radix + r'\d*)?)\s*w(?:eeks?)?'
+    if verbose > 4:
+        LOG.debug(_("Pattern {!r}.").format(pattern))
+    match = re.search(pattern, value, re.IGNORECASE)
+    if match:
+        weeks_str = match.group(1)
+        if use_locale_radix:
+            weeks_str = re.sub(radix, '.', weeks_str, 1)
+        weeks = float(weeks_str)
+        days = weeks * 7
+        if verbose > 3:
+            LOG.debug(_("Found {:f} weeks.").format(weeks))
+        value = re.sub(pattern, '', value, re.IGNORECASE)
+    if verbose > 4:
+        LOG.debug(_("Rest after weeks: {!r}.").format(value))
+
+    return (days, weeks, value)
+
+
+# =============================================================================
+def _get_months_in_period(value, radix, use_locale_radix=False, verbose=0):
+    "Search for months in value"
+
+    days = 0
+    months = 0
+
+    pattern = r'(\d+(?:' + radix + r'\d*)?)\s*m(?:onths?)?'
+    if verbose > 4:
+        LOG.debug(_("Pattern {!r}.").format(pattern))
+    match = re.search(pattern, value, re.IGNORECASE)
+    if match:
+        months_str = match.group(1)
+        if use_locale_radix:
+            months_str = re.sub(radix, '.', months_str, 1)
+        months = float(months_str)
+        days = months * 30
+        if verbose > 3:
+            LOG.debug(_("Found {:f} months.").format(months))
+        value = re.sub(pattern, '', value, re.IGNORECASE)
+    if verbose > 4:
+        LOG.debug(_("Rest after months: {!r}.").format(value))
+
+    return (days, months, value)
+
+
+# =============================================================================
+def _get_years_in_period(value, radix, use_locale_radix=False, verbose=0):
+    "Search for years in value"
+
+    days = 0
+    years = 0
+
+    pattern = r'(\d+(?:' + radix + r'\d*)?)\s*y(?:ears?)?'
+    if verbose > 4:
+        LOG.debug(_("Pattern {!r}.").format(pattern))
+    match = re.search(pattern, value, re.IGNORECASE)
+    if match:
+        years_str = match.group(1)
+        if use_locale_radix:
+            years_str = re.sub(radix, '.', years_str, 1)
+        years = float(years_str)
+        days = years * 365
+        if verbose > 3:
+            LOG.debug(_("Found {:f} years.").format(years))
+        value = re.sub(pattern, '', value, re.IGNORECASE)
+    if verbose > 4:
+        LOG.debug(_("Rest after years: {!r}.").format(value))
+
+    return (days, years, value)
+
+
+# =============================================================================
+def _get_days_in_period(value, radix, use_locale_radix=False, verbose=0):
+    "Search for days in value"
+
+    days = 0.0
+
+    pattern = r'(\d+(?:' + radix + r'\d*)?)\s*(?:d(?:ays?)?)?'
+    if verbose > 4:
+        LOG.debug(_("Pattern {!r}.").format(pattern))
+    match = re.search(pattern, value, re.IGNORECASE)
+    if match:
+        days_str = match.group(1)
+        if use_locale_radix:
+            days_str = re.sub(radix, '.', days_str, 1)
+        days = float(days_str)
+        if verbose > 3:
+            LOG.debug(_("Found {:f} days.").format(days))
+        value = re.sub(pattern, '', value, re.IGNORECASE)
+    if verbose > 4:
+        LOG.debug(_("Rest after days: {!r}.").format(value))
+
+    return (days, value)
 
 
 # =============================================================================
